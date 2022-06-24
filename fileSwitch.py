51a24f515b16416010462aa9110a5ff80d17d2cd
asent1234 <42815464+asent1234@users.noreply.github.com>
2022-06-23 17:12:55 -0700

    Add files via upload

--- fileSwitch.py
+++ fileSwitch.py
@@ -1,11 +1,18 @@
 def fileSwitch(path1, path2):
-    file1 = open(r(path1), "w+")
-    file2 = open(r(path2), "w+")
+    file1 = open(path1, "r")
+    file2 = open(path2, "r")
     contents1 = file1.readlines()
     contents2 = file2.readlines()
-    file1.writelines(contents1)
-    file2.writelines(contents2)
+    file1.close()
+    file2.close()
+    file1 = open(path1, "w")
+    file2 = open(path2, "w")
+    print(contents1)
+    print(contents2)
+    file1.writelines(contents2)
+    file2.writelines(contents1)
     file1.close()
     file2.close()
 
-fileSwitch("C:\Users\aakas\Downloads\path1.text", "C:\Users\aakas\Downloads\path2.txt")
+fileSwitch(r"C:\Users\aakas\Downloads\path1.txt", r"C:\Users\aakas\Downloads\path2.txt")
+ 
